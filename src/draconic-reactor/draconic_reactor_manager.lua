219787662f85e170f3cb82d1d155f80538fbd611
Malte Linke <me@parzival.space>
2026-07-04 04:04:47 +0200

    somewhat working output power control

--- src/draconic-reactor/draconic_reactor_manager.lua
+++ src/draconic-reactor/draconic_reactor_manager.lua
@@ -13,10 +13,16 @@ local DraconicReactorManager = {
 
     reactor_state = ReactorState.COLD,
 
-    -- todo: output goal
+    output_flow = 0,
+
+    temperature_goal = 7000,
+    temperature = 0,
+
+    -- PID state
+    _cap_pid = nil
 }
 
-function DraconicReactorManager:new(o, draconic_reactor, input_flux_gate, output_flux_gate)
+function DraconicReactorManager:new(o, draconic_reactor, input_flux_gate, output_flux_gate, output_energy_detector)
     -- class constructor
     o = o or {}
     setmetatable(o, self)
@@ -38,7 +44,9 @@ function DraconicReactorManager:handle()
             goto continue
         end
 
-        -- todo: update reactor values
+        -- update output flow
+        self.output_flow = reactor_info.generationRate
+        self.temperature = reactor_info.temperature
 
         -- handle reactor based on its current state
         self.reactor_state = reactor_info.status
@@ -53,7 +61,7 @@ function DraconicReactorManager:handle()
         elseif self.reactor_state == ReactorState.COOLING then
             self:_handle_cooling()
         else
-            print("Unexpected Reactor state: " .. self.reactor_state)
+            print("Unexpected Reactor state: " .. tostring(self.reactor_state))
         end
 
         sleep(0.2)
@@ -62,18 +70,18 @@ function DraconicReactorManager:handle()
 end
 
 function DraconicReactorManager:_handle_cold()
-
+    -- todo: implement
 end
 
 function DraconicReactorManager:_handle_warming_up()
     -- speed up warm up by increasing the input flow
-    if self.input_flux_gate.getSignalLowFlow() < 900000 then
+    if self.input_flux_gate.getSignalLowFlow() ~= 900000 then
         print("Updating input flow to 900000 RF/t")
         self.input_flux_gate.setSignalLowFlow(900000)
     end
 
     -- disable output during warmup for safety
-    if self.output_flux_gate.getSignalLowFlow() > 0 then
+    if self.output_flux_gate.getSignalLowFlow() ~= 0 then
         print("Disabling output flow during warm up")
         self.output_flux_gate.setSignalLowFlow(0)
     end
@@ -92,14 +100,43 @@ function DraconicReactorManager:_handle_running()
     -- auto adjust input flow
     -- this will prevent the reactor going nuclear even if the temperature reaches max.
     -- the code below expects that infinite energy is available to power the shield
-    local current_flux_flow = self.input_flux_gate.getFlow()
-    local needed_flux_flow = reactor_info.fieldDrainRate / (1 - (30 / 100)) -- todo: replace 50 with configurable value
-    if current_flux_flow ~= needed_flux_flow then
-        print("Updating input flux rate " .. current_flux_flow .. " RF/t -> " .. needed_flux_flow .. " RF/t")
-        self.input_flux_gate.setSignalLowFlow(needed_flux_flow)
+    local current_input_flux_flow = self.input_flux_gate.getFlow()
+    local needed_input_flux_flow = reactor_info.fieldDrainRate / (1 - (20 / 100)) -- todo: replace 20 with configurable value
+    if current_input_flux_flow ~= needed_flux_flow then
+        print("Updating input flux rate " .. current_input_flux_flow .. " RF/t -> " .. needed_input_flux_flow .. " RF/t")
+        self.input_flux_gate.setSignalLowFlow(needed_input_flux_flow)
     end
 
-    -- todo: slowly rise until temperature goal
+    -- when the reactor is in a stable state, gradually increase the max output flow rate.
+    -- if the max output flow is instantly reached, this means the consuming network demands more energy
+    -- which in turn means we need to increase the reactor output further
+    local current_max_output_flow = self.output_flux_gate.getSignalLowFlow()
+    local current_output_flow = reactor_info.generationRate
+    local current_temperature = reactor_info.temperature
+
+    -- local current_energy_saturation = reactor_info.energySaturation / 1000000000
+    -- if current_output_flow >= (current_max_output_flow * 0.9) and current_energy_saturation > 0.8 then
+    if (current_output_flow >= (current_max_output_flow * 0.99) and current_temperature < 7000) or current_max_output_flow <= 0 then
+        -- increase max output, energy demands are meet
+        local new_max_output_flow = current_max_output_flow + 1000
+
+        print("Updating output flux rate " .. current_max_output_flow .. " RF/t -> " .. new_max_output_flow .. " RF/t")
+        self.output_flux_gate.setSignalLowFlow(new_max_output_flow)
+    -- elseif current_energy_saturation < 0.5 then
+    --     -- decrease max output, energy demands are too high
+    --     local new_max_output_flow = current_max_output_flow - 10000
+
+    --     print("Updating output flux rate " .. current_max_output_flow .. " RF/t -> " .. new_max_output_flow .. " RF/t")
+    --     self.output_flux_gate.setSignalLowFlow(new_max_output_flow)
+    end
+
+
+    -- safety: shutdown if overheating
+    local MAX_TEMPERATURE = 8000
+    if reactor_info.temperature >= MAX_TEMPERATURE then
+        print("Failsafe: Reactor reached a temperature higher than " .. MAX_TEMPERATURE .. ", shutting down")
+        self.reactor.stopReactor()
+    end
 end
 
 function DraconicReactorManager:_handle_stopping()
--- src/draconic-reactor/main.lua
+++ src/draconic-reactor/main.lua
@@ -9,12 +9,12 @@ local input_flux_gate = peripheral_store:load_or_find("input_flux_gate", "flow_g
     -- input gate is the gate with low flow set to 10
     print(name .. " has flow state " .. flow_gate.getSignalLowFlow())
     return flow_gate.getSignalLowFlow() == 10
-end)
+end) or error("Reactor input Flux Gate not found! Make sure the 'Redstone Signal Low' flow is set to 10 RF/t.")
 local output_flux_gate = peripheral_store:load_or_find("output_flux_gate", "flow_gate", function(name, flow_gate)
     -- input gate is the gate with low flow is not set to 10
     print(name .. " has flow state " .. flow_gate.getSignalLowFlow())
     return flow_gate.getSignalLowFlow() ~= 10
-end)
+end) or error("Reactor output Flux Gate not found!")
 
 -- init managers
 local reactor_manager = DraconicReactorManager:new(nil, draconic_reactor, input_flux_gate, output_flux_gate)
